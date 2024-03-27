86606cda2d24e193a5edf00c34ea70abd0c31999
annie <annieyanghy@gmail.com>
2024-03-27 09:43:37 -0400

    process card fixing clicking anchor tag

--- components/blocks/projectProcess.js
+++ components/blocks/projectProcess.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function ProcessCard(props) {
     return (
-
+        <a  href={'#'.concat(props.title)} >
         <div
             className="
                     group 
                     bg-slate-700/80  
                     p-3
                     m-2
-                    w-fit
-                    h-48
+                    w-full
+                    
                     justify-center
                     md:flex-auto
 
@@ -23,7 +23,7 @@ export default function ProcessCard(props) {
                     hover:cursor-pointer
                     hover:bg-slate-900
                     ">
-            <a  href={'#'+props.title} >
+            
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row items-ceter space-x-2">
                         <span class="material-symbols-rounded" style={{ fontSize: 20 }}>
@@ -47,19 +47,18 @@ export default function ProcessCard(props) {
                         style={{ fontSize: 32 }}>
                         chevron_right</span>
                 </div>
-
+                <div className="h-24">
                 {props.description?.map((i) => (
                     <p className="
                         text-sm
                         text-zinc-100
-                       
                         ">
                         * {i}
                     </p>
                 ))}
-               
-            </a>
+               </div>
+           
         </div>
-
+        </a>
     );
 }
